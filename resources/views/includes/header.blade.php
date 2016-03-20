4e312bbec61449b21f7ddbc5af1d1a66c407bb44
Matthew B <matthew@bluevfx.com>
2016-03-20 22:01:07 +0200

    Added ajax call

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,8 +1,6 @@
 <title>Stock</title>
 
-<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/style.css') }}">
-
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery1.11.3/jquery.min.js"></script>
+<script src="https://code.jquery.com/jquery-2.2.2.min.js" integrity="sha256-36cp2Co+/62rEAAYHLmRCPIych47CvdM+uTBJwSzWjI=" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
--- resources/views/products/stock.blade.php
+++ resources/views/products/stock.blade.php
@@ -5,7 +5,7 @@
 	<div class="col-lg-12">
 		<div class="row">
 			<h3>Add Stock</h3>
-			<form class="form-horizontal" action="/add" method="POST">
+			<form class="form-horizontal" id="add_product" action="/add" method="POST">
 				{{ csrf_field() }}
 
 				<div class="form-group">
@@ -35,8 +35,27 @@
 		</div>
 	</div>
 </div>
+<div class="row">
+	<div class="col-lg-12" id="results">
+		
+	</div>
+</div>
 <script type="text/javascript">
-	
+$('#add_product').submit(function(e){
+	e.preventDefault();
 
+	$.ajax({
+		url: '/add',
+		type: 'POST',
+		data: $('#add_product').serialize(),
+		dataType: 'JSON',
+		success: function(data){
+			//console.log(data);
+			$('#results').append(
+				'<span class="col-lg-12">'+data.name+' | '+data.quantity+' | '+data.price+' | 1 March 2016 </span>'
+			);
+		}
+	});
+});
 </script>
 @stop
